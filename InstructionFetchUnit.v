3a26378ce9c66a48a2dcf7d0fea6529d41ec85be
mariow-ops <104529480+mariow-ops@users.noreply.github.com>
2022-08-31 14:56:48 -0700

    Update InstructionFetchUnit.v

--- InstructionFetchUnit.v
+++ InstructionFetchUnit.v
@@ -37,18 +37,28 @@
 // which generates a continuous clock pulse into the module.
 ////////////////////////////////////////////////////////////////////////////////
 
-module InstructionFetchUnit(Instruction, Reset, Clk);
-   
+module InstructionMemory(Address, Instruction); 
 
-	input Reset, Clk;
-	output reg [31:0] Instruction;
-	output reg [31:0] PCResult;
-	
-	wire [31:0] PCAddResult;
-	
-	 
-	PCAdder adder(PCResult,PCAddResult);
-	ProgramCounter PC(PCAddResult,PCResult,Reset,Clk);
-	InstructionMemory memory(PCResult,Instruction);
-    /* Please fill in the implementation here... */
+    integer i;
+    input [31:0] Address;        // Input Address 
+    
+    reg [31:0] mem [127:0];
+
+    output reg [31:0] Instruction;    // Instruction at memory location Address
+    
+    initial begin
+      for (i = 0; i < 128; i = i + 1) begin
+            mem[i] <= i * 3;
+      end
+    end
+    
+    always @ (Address) begin
+    
+    //initial begin
+       
+     Instruction <= mem[Address];
+    end
+        
+        
+
 endmodule
